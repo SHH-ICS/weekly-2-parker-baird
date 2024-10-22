2880f117b66de2f0ce8cd089f161027d7c2602b7
parker-baird <parker.baird-ashmore@stu.ocsb.ca>
2024-10-22 14:18:18 +0000

    hh

--- assignment.py
+++ assignment.py
@@ -1,1 +1,12 @@
+import math
 
+print("What is the Diameter?")
+D = float(input())
+radius = D / 2
+A = math.pi * radius ** 2
+C = 2 * math.pi * radius
+
+print("The Area is...")
+print(A)
+print("The Circumference is...")
+print(C)
